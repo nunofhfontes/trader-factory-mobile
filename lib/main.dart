a292f3669c88deca8e3ec2ea8df18ad0c501c5e6
nunofontes <nunof.h.fontes@gmail.com>
2020-12-06 23:35:31 +0000

    added bordered text dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:bordered_text/bordered_text.dart';
+
 void main() {
   runApp(TraderFactoryApp());
 }
@@ -8,61 +10,47 @@ class TraderFactoryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'TraderFactory App',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        scaffoldBackgroundColor: Colors.black,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'TraderFactory'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      title: 'Trader Factory',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.green,
+      //   scaffoldBackgroundColor: Colors.white30,
+      //   visualDensity: VisualDensity.adaptivePlatformDensity,
+      // ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(#06f71b),
+          title: Center(child:
+            Text('Trader Factory'),
+          )
+        ),
+        body: Center(
+          child: BorderedText(
+            strokeWidth: 1.0,
+            child: Text(
+              'Trader Factory',
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                decorationColor: Colors.green,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: TextStyle(color: Colors.white),
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+
+    // return MaterialApp(
+    //   title: 'TraderFactory App',
+    //   theme: ThemeData(
+    //     primarySwatch: Colors.green,
+    //     scaffoldBackgroundColor: Colors.black,
+    //     visualDensity: VisualDensity.adaptivePlatformDensity,
+    //   ),
+    //   home: MyHomePage(title: 'TraderFactory'),
+    // );
   }
 }
+
+
