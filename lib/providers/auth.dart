57a88e177dd8941a49d940885e7fd01dcbf83927
nunofontes <nunof.h.fontes@gmail.com>
2020-12-08 00:49:57 +0000

    new auth endpoint

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -26,7 +26,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url =
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyC13spCwP_f_SalxEbkB-wjedoF8iYENlQ';
+        'https://localhost:3000/api/auth';
     try {
       final response = await http.post(
         url,
